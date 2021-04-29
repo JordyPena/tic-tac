b610af5f0dedc3615f82eff87f6db6b097cca795
Jordy Pena <jordypenamontes@gmail.com>
2021-04-29 14:17:11 -0500

    board.js created state for squares and current player
    conditional status
    
    method to handleClick
    
    added new props to Square component

--- src/Board.js
+++ src/Board.js
@@ -1,18 +1,39 @@
 import Square from "./Square.js"
+import { useState } from "react";
 
 const Board = () => {
+  //initial state containing an array of 9 nulls
+  //corresponding to the 9 squares
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
 
-  const status = "Next player: X";
+  const status = (xIsNext ? "Player1 (X) where would you like to move?" : "Player2 (O) where would you like to move?");
 
+  const spaceClicked = (i) => {
+    // use slice to create a copy of the squares array to modify 
+    //insteaad of modifying the existing array.
+    const newSquares = squares.slice();
+    newSquares[i] = xIsNext ? 'X' : 'O';
+    setSquares(newSquares);
+    setXIsNext(!xIsNext);
+    
+  }
 
   const renderSquare = (index) => {
-    return <Square/>
+    return (
+
+      <Square value={squares[index]} onClick={() => spaceClicked(index)} />
+    )
+      
+      
   }
+
   return (
     <div>
       <h3>Welcome! here is your board</h3>
       <div className="status">{status}</div>
       <div className="board-row">
+        {/* calling renderSquare function and passing a number as a param */}
         {renderSquare(0)}
         {renderSquare(1)}
         {renderSquare(2)}
